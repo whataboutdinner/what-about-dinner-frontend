e215ed67f038194afae18ffcc4566b9bda86fbbd
whataboutdinner <nicdeme48@gmail.com>
2025-05-01 13:13:52 -0400

    Update layout.tsx
    simply root layout for testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,13 @@
 "use client";
 
-import { AuthProvider } from '@/lib/auth-context';
-import { PartyProvider } from '@/lib/party-context';
-import Header from '@/components/Header';
+import React from 'react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode })  {
   return (
     <html lang="en">
-      <body
-        <Header />
-        <PartyProvider>
-          <AuthProvider>
-            {children}
-          </AuthProvider>
-        </PartyProvider>
+      <body>
+        {children}
       </body>
     </html>
-  )
+  );
 }
